94b9cb7d4e7c360a48782b90629f648ef0565aa9
“lucifer2355” <dgajjar999@gmail.com>
2021-04-03 22:14:58 +0530

    add search in chat input

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ChatIcon from "@material-ui/icons/Chat";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Container = styled.div``;
 
@@ -13,14 +14,33 @@ const Header = styled.div`
   background-color: white;
   z-index: 1;
   justify-content: space-between;
+  align-items: center;
   padding: 15px;
   height: 80px;
   border-bottom: 1px solid whitesmoke;
 `;
 
-const UserAvatar = styled(Avatar)``;
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
 
 const IconContainer = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border-width: 0;
+`;
 
 const Sidebar = () => {
   return (
@@ -37,6 +57,11 @@ const Sidebar = () => {
           </IconButton>
         </IconContainer>
       </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder='Search in chats' />
+      </Search>
     </Container>
   );
 };
