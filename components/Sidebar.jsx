545377e5e6f3d97f4a095a7f22e6d8e04d2798af
“lucifer2355” <dgajjar999@gmail.com>
2021-04-03 22:04:39 +0530

    add icon and some icon style

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
 
 const UserAvatar = styled(Avatar)``;
 
@@ -16,7 +28,14 @@ const Sidebar = () => {
       <Header>
         <UserAvatar />
 
-        <IconContainer></IconContainer>
+        <IconContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconContainer>
       </Header>
     </Container>
   );
