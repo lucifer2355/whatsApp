6e504451b2fa59b0c34cf09584be88e854380748
“lucifer2355” <dgajjar999@gmail.com>
2021-04-03 23:00:37 +0530

    add useAuthState hook for check use is login or not

---
+++ pages/login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Head from "next/head";
+
+const Container = styled.div``;
+
+const Login = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+    </Container>
+  );
+};
+
+export default Login;
