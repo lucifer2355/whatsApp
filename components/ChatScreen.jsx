19ccff5d54d8b210e3814bf1f450f9c6c7ccced7
“lucifer2355” <dgajjar999@gmail.com>
2021-04-04 15:51:19 +0530

    add chat screen

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -4,7 +4,11 @@ import styled from "styled-components";
 const Container = styled.div``;
 
 const ChatScreen = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <h1>This is a chat</h1>
+    </Container>
+  );
 };
 
 export default ChatScreen;
