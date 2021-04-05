73cee3f69450fed8b4d81e21b8a18a91e5f2c2d8
“lucifer2355” <dgajjar999@gmail.com>
2021-04-05 15:33:15 +0530

    complete the chat header style

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -10,11 +10,36 @@ import { auth } from "../firebase";
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 10;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
 
-const HeaderInformation = styled.div``;
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 14px;
+    color: gray;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
 
 const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div``;
 
 const ChatScreen = ({ chat, message }) => {
   const [user] = useAuthState(auth);
@@ -38,6 +63,11 @@ const ChatScreen = ({ chat, message }) => {
           </IconButton>
         </HeaderIcons>
       </Header>
+
+      <MessageContainer>
+        {/* //* Show messages */}
+        <EndOfMessage />
+      </MessageContainer>
     </Container>
   );
 };
