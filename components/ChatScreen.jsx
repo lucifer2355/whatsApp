6972dc64e155c4d06714ddf631df4c9ed424ebdf
“lucifer2355” <dgajjar999@gmail.com>
2021-04-12 12:39:28 +0530

    add send message function

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
+import firebase from "firebase";
 import styled from "styled-components";
 import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
@@ -103,6 +104,27 @@ const ChatScreen = ({ chat, message }) => {
 
   const sendMessage = (e) => {
     e.preventDefault();
+
+    db.collection("users")
+      .doc(user.uid)
+      .set(
+        {
+          lastSceen: firebase.firestore.FieldValue.serverTimestamp(),
+        },
+        { merge: true }
+      );
+
+    db.collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        user: user.email,
+        photoURL: user.photoURL,
+      });
+
+    setInput("");
   };
 
   return (
